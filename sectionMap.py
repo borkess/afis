03a8c1a0c704bc10ff42d33af5a00f66f4ec11ee
borkess <dbohorquezk@gmail.com>
2015-01-02 21:31:49 -0500

    rellenar blobs

--- sectionMap.py
+++ sectionMap.py
@@ -30,40 +30,45 @@ map.findCenterOfMass()
 #-------------------------------------------------------------------------------
 comX, comY = map.centerOfMass
 
-def encontrarOffsetX(offset, map, comX, comY, sign, limit):
+def encontrarOffsetX(offset, map, comX, comY, sign, limit, bottom, top):
     edge = False
     while not edge:
         offset += 1
+        x = comX + offset*sign
         for i in range(offset + 1):
-            if (map.foreground[comX+offset*sign][comY-i] or
-                map.foreground[comX+offset*sign][comY+i]):
+            yBottom = bottom if comY-i < bottom else comY-i
+            yTop = top if comY+i > top else comY+i
+            if (map.foreground[x][yBottom] or map.foreground[x][yTop]):
                 break
-            elif comX+offset*sign == limit:
-                edge = True
             elif i == offset:
                 offset -= 1
                 edge = True
+            elif x == limit:
+                edge = True
     return offset
-def encontrarOffsetY(offset, map, comX, comY, sign, limit):
+def encontrarOffsetY(offset, map, comX, comY, sign, limit, left, right):
     edge = False
     while not edge:
         offset += 1
+        y = comY + offset*sign
         for j in range(offset + 1):
-            if (map.foreground[comX-j][comY+offset*sign] or
-                map.foreground[comX+j][comY+offset*sign]):
+            xLeft = left if comX-j < comX-j else comX-j
+            xRight = right if comX+j > right else comX+j
+            if (map.foreground[xLeft][y] or
+                map.foreground[xRight][y]):
                 break
-            elif comX+offset*sign == limit:
-                edge = True
             elif j == offset:
                 offset -= 1
+                edge = True
+            elif y == limit:
                 edge = True
     return offset
     
 
-offsetW = encontrarOffsetX(10, map, comX, comY, -1, 0)
-offsetE = encontrarOffsetX(10, map, comX, comY, 1, map.width-1)
-offsetS = encontrarOffsetY(2, map, comX, comY, -1, 0)
-offsetN = encontrarOffsetY(10, map, comX, comY, 1, map.height-1)
+offsetW = encontrarOffsetX(10, map, comX, comY, -1, 0, 0, map.height-1)
+offsetE = encontrarOffsetX(10, map, comX, comY, 1, map.width-1, 0, map.height-1)
+offsetS = encontrarOffsetY(2, map, comX, comY, -1, 0, 0, map.width-1)
+offsetN = encontrarOffsetY(10, map, comX, comY, 1, map.height-1, 0, map.width-1)
 
 left = 0 if comX < offsetW else comX-offsetW
 right = map.width-1 if comX+offsetE >= map.width else comX+offsetE
@@ -73,40 +78,40 @@ top = map.height-1 if comY+offsetN >= map.height else comY+offsetN
 #"""
 for x in range(map.width):
     for y in range(map.height):
-        if x not in range(left, right) or y not in range(bottom, top):
+        if (x < left or  x > right) or (y < bottom or y > top):
             map.foreground[x][y] = False
 
-for y in range(bottom, top):
-    xl = left
-    xr = right
+for y in range(bottom, top+1):
+    xl = right
+    xr = left
     xlf = False
     xrf = False
     for x in range(right-left):
+        if xlf and xrf:
+            break
         if map.foreground[left+x][y] and not xlf:
             xl = left+x
             xlf = True
         if map.foreground[right-x][y] and not xrf:
             xr = right-x
             xrf = True
-        if xlf and xrf:
-            break
     for x in range(xl, xr+1):
         map.foreground[x][y] = True
 
-for x in range(left, right):
+for x in range(left, right+1):
     yb = bottom
     yt = top
     ybf = False
     ytf = False
     for y in range(top-bottom):
+        if ybf and ytf:
+            break
         if map.foreground[x][bottom+y] and not ybf:
             yb = bottom+y
             ybf = True
         if map.foreground[x][top-y] and not ytf:
             yt = top-y
             ytf = True
-        if ybf and ytf:
-            break
     for y in range(yb, yt+1):
         map.foreground[x][y] = True
 """
@@ -123,17 +128,18 @@ for x in range(map.width):
 map.cleanBackground()
 
 #-------------------------------------------------------------------------------
-print("centerOfMass: ", map.centerOfMass)
+print("centerOfMass:", map.centerOfMass)
 map.paintBlock(comX, comY, (0, 0, 255))
 
-print("offsetW: ", offsetW, "offsetE: ", offsetE, "offsetS: ", offsetS, "offsetN: ", offsetN)
-print("left: ", left, "bottom: ", bottom, "right: ", right, "top: ", top)
+print("offsetW:", offsetW, "offsetE:", offsetE, "offsetS:", offsetS, "offsetN:", offsetN)
+print("left:", left, "bottom:", bottom, "right:", right, "top:", top)
 map.paintBlock(left, bottom, (0, 0, 255))
 map.paintBlock(right, bottom, (0, 0, 255))
 map.paintBlock(left, top, (0, 0, 255))
 map.paintBlock(right, top, (0, 0, 255))
 #-------------------------------------------------------------------------------
 
-bmp.makeFile(path + image + "." + str(map.threshold) + ".bmp")
+bmp.makeFile(path + image + ".procesada.bmp")
+print(path + image + ".procesada.bmp")
 
 
